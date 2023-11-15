e8abde07a15f31606b6ee0f8cf180049ddd1c14e
Renan Joppert <renanjoppert@outlook.com>
2023-11-14 21:09:48 -0300

    Tela de login

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    <slot />
+  <div class="wrapper tw-flex">
+    <div
+      class="image-wrapper tw-hidden md:tw-flex md:tw-w-2/3 tw-flex tw-min-h-screen tw-justify-center tw-items-center tw-p-10"
+    >
+      <img src="@/assets/img/logo.svg" class="tw-absolute tw-top-5 tw-left-5" />
+      <img src="@/assets/img/illustration.svg" />
+    </div>
+    <div
+      class="content-wrapper tw-bg-white tw-grow tw-flex tw-h-screen tw-items-center tw-box-border tw-p-10"
+    >
+      <div class="tw-w-full">
+        <img
+          src="@/assets/img/logo.svg"
+          class="tw-block md:tw-hidden tw-mb-4"
+        />
+        <slot />
+      </div>
+    </div>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,46 @@
 <template>
-  <div>oi</div>
+  <div>
+    <p class="tw-text-lg tw-font-semibold tw-mb-3">Welcome to Vuexy! 👋🏻</p>
+    <p>Please sign-in to your account and start the adventure</p>
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-lg">
+      <q-input v-model="email" label="E-mail" />
+
+      <q-input
+        v-model="password"
+        :type="isPwd ? 'password' : 'text'"
+        label="Senha"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+
+      <q-btn label="Entrar" type="submit" color="primary" class="tw-w-full" />
+    </q-form>
+    <a href="#" class="tw-mt-4 tw-block tw-text-brand-primary tw-font-medium"
+      >Esqueceu sua senha?</a
+    >
+  </div>
 </template>
 
 <script lang="ts" setup>
 definePageMeta({
   layout: "auth",
 });
+
+const router = useRouter();
+
+const email = ref("");
+const password = ref("");
+const isPwd = ref(true);
+
+function onSubmit() {
+  router.push("/dashboard");
+}
 </script>
 
 <style>
